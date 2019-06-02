82542a597db89cce71d46e482979c7f212f5dced
rommguy <rommguy@gmail.com>
2019-06-02 16:17:34 +0300

    Add survey demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { FunctionComponent } from 'react'
 import './App.scss'
+import MaterialTextField from '@material-ui/core/TextField'
+
+const SurveyQuestion: FunctionComponent<{}> = () => (
+  <div className="question-container">
+    <div className="question-title">Why like this?</div>
+    <div className="answer-container">
+      <MaterialTextField placeholder="Please answer" />
+    </div>
+  </div>
+)
+
+const questionsArray: number[] = new Array(50).fill(1)
 
 function App() {
   return (
     <div className="App">
-      <h2 className="title">Bugs recreation page - Romm Guy</h2>
+      <h2 className="title">Bugs recreation page</h2>
+      <div className="shiulon">
+        <div className="questions-container">
+          {questionsArray.map((val, index) => (
+            <SurveyQuestion key={index} />
+          ))}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
