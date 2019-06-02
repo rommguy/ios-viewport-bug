ffe31dfc375670be9050b608da48730eae4d7868
rommguy <rommguy@gmail.com>
2019-06-02 15:58:17 +0300

    add scss and typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="title">Bugs recreation page - Romm Guy</h2>
+    </div>
+  );
+}
+
+export default App;
