35ca7d2490e82ff243fd7768d26b1dc5a3cee181
rommguy <rommguy@gmail.com>
2019-06-03 09:35:00 +0300

    bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,26 @@ import React, { FunctionComponent } from 'react'
 import './App.scss'
 import MaterialTextField from '@material-ui/core/TextField'
 
-const SurveyQuestion: FunctionComponent<{}> = () => (
+const SurveyQuestion: FunctionComponent<{ onBlur: () => void }> = ({ onBlur }) => (
   <div className="question-container">
     <div className="question-title">Why like this?</div>
     <div className="answer-container">
-      <MaterialTextField placeholder="Please answer" />
+      <MaterialTextField placeholder="Please answer" onBlur={onBlur} />
     </div>
   </div>
 )
 
 const questionsArray: number[] = new Array(50).fill(1)
 
-function App() {
+const App: FunctionComponent<{}> = () => {
+  const onInputBlur = () => null
   return (
     <div className="App">
       <h2 className="title">Bugs recreation page</h2>
       <div className="shiulon">
         <div className="questions-container">
           {questionsArray.map((val, index) => (
-            <SurveyQuestion key={index} />
+            <SurveyQuestion key={index} onBlur={onInputBlur} />
           ))}
         </div>
       </div>
